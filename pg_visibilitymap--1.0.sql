fcacd4d84b325a026f2ca0f6c89919afe709c550
Masahiko Sawada <sawada.mshk@gmail.com>
2015-11-17 11:44:44 +0900

    Fix typo in SQL file.

--- pg_visibilitymap--1.0.sql
+++ pg_visibilitymap--1.0.sql
@@ -1,4 +1,4 @@
-/* contrib/pg_visibilitymap/pg_visibilitymap--1.0.sql */
+/* pg_visibilitymap/pg_visibilitymap--1.0.sql */
 
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION pg_visibilitymap" to load this file. \quit
@@ -8,8 +8,7 @@ RETURNS bool
 AS 'MODULE_PATHNAME', 'pg_is_all_visible'
 LANGUAGE C VOLATILE STRICT;
 
-CREATE FUNCTION
-  pg_visibilitymap(rel regclass, blkno OUT bigint, all_visible OUT bool)
+CREATE FUNCTION pg_visibilitymap(rel regclass, blkno OUT bigint, all_visible OUT bool)
 RETURNS SETOF RECORD
 AS $$
   SELECT blkno, pg_is_all_visible($1, blkno) AS all_visible 
