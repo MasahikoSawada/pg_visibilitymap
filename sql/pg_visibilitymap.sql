993d358dc9e9c3af003cee35c63780bfd24e4b5a
Masahiko Sawada <sawada.mshk@gmail.com>
2015-11-18 10:04:24 +0900

    Add regression test for errors.
    Additional regression test is for errors test regarding relation type.
    Also, unify the SQLs upper case latter in regression test.

--- sql/pg_visibilitymap.sql
+++ sql/pg_visibilitymap.sql
@@ -1,16 +1,24 @@
 create extension pg_visibilitymap;
 
-create table t1 as select * from generate_series(1,1000) as p;
+CREATE TABLE t1 AS SELECT * FROM generate_series(1,1000) AS p;
 \d t1
-vacuum t1;
-select relpages from pg_class where relname = 't1';
+VACUUM t1;
+SELECT relpages FROM pg_class WHERE relname = 't1';
+SELECT * FROM pg_visibilitymap('t1');
+DELETE FROM t1 WHERE p < 500;
+SELECT * FROM pg_visibilitymap('t1');
+VACUUM t1;
+SELECT * FROM pg_visibilitymap('t1');
+DROP TABLE t1;
 
-select * from pg_visibilitymap('t1');
-
-delete from t1 where p < 500;
-
-select * from pg_visibilitymap('t1');
-
-vacuum t1;
-
-select * from pg_visibilitymap('t1');
+--
+-- Errors
+--
+CREATE TABLE t2 (col int primary key);
+INSERT INTO t2 SELECT generate_series(1,100);
+CREATE VIEW t2_view AS (SELECT * FROM t2);
+CREATE MATERIALIZED VIEW t2_matview AS (SELECT * FROM t2);
+SELECT pg_is_all_visible('t2', 0);
+SELECT pg_is_all_visible('t2_view', 0);
+SELECT pg_is_all_visible('t2_matview', 0);
+DROP TABLE t2 CASCADE;
